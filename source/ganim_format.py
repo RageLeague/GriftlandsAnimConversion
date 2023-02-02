9596a548cd6dac7f48ecbb4d9144de4d55511d94
RageLeague <34314733+RageLeague@users.noreply.github.com>
2023-02-02 10:11:07 -0500

    Use dataclass

--- source/ganim_format.py
+++ source/ganim_format.py
@@ -1,13 +1,14 @@
+from dataclasses import dataclass, field
 BUILD_VERSION = 10
 ANIM_VERSION = 7
 
 def to_hex(int_val: int) -> str:
     return "0x{0:08X}".format(int_val)
 
+@dataclass
 class Coord:
-    def __init__(self, x = 0.0, y = 0.0) -> None:
-        self.x = x
-        self.y = y
+    x: float = 0.0
+    y: float = 0.0
 
     def __str__(self) -> str:
         return f"({self.x}, {self.y})"
@@ -15,10 +16,10 @@ class Coord:
     def __repr__(self) -> str:
         return str(self)
 
+@dataclass
 class BBox:
-    def __init__(self, pos: Coord | None = None, size: Coord | None = None) -> None:
-        self.pos = pos or Coord()
-        self.size = size or Coord()
+    pos: Coord = field(default_factory=Coord)
+    size: Coord = field(default_factory=Coord)
 
     def __str__(self) -> str:
         return f"[pos={self.pos}, size={self.size}]"
@@ -26,10 +27,10 @@ class BBox:
     def __repr__(self) -> str:
         return str(self)
 
+@dataclass
 class HashedString:
-    def __init__(self, hash_val = 0, original = "") -> None:
-        self.hash_val = hash_val
-        self.original = original
+    hash_val: int = 0
+    original: str = ""
 
     def __str__(self) -> str:
         return f"'{self.original}'[{to_hex(self.hash_val)}]"
@@ -37,30 +38,21 @@ class HashedString:
     def __repr__(self) -> str:
         return str(self)
 
+@dataclass
 class BuildFrame:
-    def __init__(
-        self,
-        frame_num = 0,
-        duration = 0,
-        image_index = 0,
-        bbox: BBox | None = None,
-        uv0: Coord | None = None,
-        uv1: Coord | None = None
-    ) -> None:
-        self.frame_num = frame_num
-        self.duration = duration
-        self.image_index = image_index
-        self.bbox = bbox or BBox()
-        self.uv0 = uv0 or Coord()
-        self.uv1 = uv1 or Coord()
+    frame_num: int = 0
+    duration: int = 0
+    image_index: int = 0
+    bbox: BBox = field(default_factory=BBox)
+    uv0: Coord = field(default_factory=Coord)
+    uv1: Coord = field(default_factory=Coord)
 
-
+@dataclass
 class BuildSymbol:
-    def __init__(self, symbol_hash = 0, color_channel_hash = 0, looping = False, frames: list[BuildFrame] | None = None) -> None:
-        self.symbol_hash = symbol_hash
-        self.color_channel_hash = color_channel_hash
-        self.looping = looping
-        self.frames = frames or []
+    symbol_hash: int = 0
+    color_channel_hash: int = 0
+    looping: bool = False
+    frames: list[BuildFrame] = field(default_factory=list)
 
     def __str__(self) -> str:
         return f"Symbol {to_hex(self.symbol_hash)} ({len(self.frames)} frames)"
@@ -68,15 +60,15 @@ class BuildSymbol:
     def __repr__(self) -> str:
         return str(self)
 
+@dataclass
 class BuildFile:
-    def __init__(self, version = BUILD_VERSION, total_frames = 0, build_name = "", materials: list[str] | None = None, sdf_materials: list[str] | None = None, symbols: list[BuildSymbol] | None = None, hashed_strings: list[HashedString] | None = None) -> None:
-        self.version = version
-        self.total_frames = total_frames
-        self.build_name = build_name
-        self.materials = materials or []
-        self.sdf_materials = sdf_materials or []
-        self.symbols = symbols or []
-        self.hashed_strings = hashed_strings or []
+    version: int = BUILD_VERSION
+    total_frames: int = 0
+    build_name: str = ""
+    materials: list[str] = field(default_factory=list)
+    sdf_materials: list[str] = field(default_factory=list)
+    symbols: list[BuildSymbol] = field(default_factory=list)
+    hashed_strings: list[HashedString] = field(default_factory=list)
 
     def __str__(self) -> str:
         return f"Build '{self.build_name}' ({len(self.symbols)} symbols)"
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from source.ganim_io import GriftAnimIO
-with open(r'raw_files\anim.zip\build.bin', 'rb') as file:
+with open(r'raw_files/anim.zip/build.bin', 'rb') as file:
     result = GriftAnimIO.read_build_file(file)
     print(result)
-    with open(r'raw_files\anim.zip\build_copy.bin', 'wb') as file:
+    with open(r'raw_files/anim.zip/build_copy.bin', 'wb') as file:
         GriftAnimIO.write_build_file(file, result)
