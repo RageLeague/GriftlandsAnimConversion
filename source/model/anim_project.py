4991f96f3a4881e8a852b8473fca3211fecc8bf1
RageLeague <34314733+RageLeague@users.noreply.github.com>
2023-04-09 21:58:26 -0400

    Create anim_project.py

---
+++ source/model/anim_project.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass, field
+from PIL import Image
+from typing import Optional
+
+@dataclass
+class IntCoord:
+    x: int = 0
+    y: int = 0
+
+@dataclass
+class HasUID:
+    _uid: int = 0
+
+@dataclass
+class AtlasImage(HasUID):
+    image: Optional[Image.Image] = None
+    # Position of top left corner of image within the atlas
+    pos: IntCoord = field(default_factory=IntCoord)
+    # Optional name for the image to better identify it
+    name: str = ""
+
+@dataclass
+class Atlas(HasUID):
+    # Dict of images based on uid
+    images: dict[int, AtlasImage] = field(default_factory=dict)
+    # Size of the atlas
+    size: IntCoord = field(default_factory=IntCoord)
+    # Name for the atlas
+    name: str = ""
+
+@dataclass
+class AnimProject:
+    # Dict of atlases based on uid
+    atlases: dict[int, Atlas] = field(default_factory=dict)
+
+    _current_uid: int = 0
+    def get_new_uid(self):
+        self._current_uid += 1
+        return self._current_uid
