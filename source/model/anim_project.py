8fe601d24f73e4717d929635f79a1376a78c7eff
RageLeague <34314733+RageLeague@users.noreply.github.com>
2023-04-24 18:18:37 -0400

    Remove uidref

--- source/model/anim_project.py
+++ source/model/anim_project.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass, field
 from PIL import Image
-from typing import Optional, TypeVar, Generic, Any
-
-T = TypeVar("T")
+from typing import Optional, TypeVar
 
 @dataclass
 class IntCoord:
@@ -16,25 +14,9 @@ class HasUID:
 U = TypeVar("U", bound=HasUID)
 
 @dataclass
-class UIDRef(Generic[T]):
-    obj_type: type[T] = field()
-    project: 'AnimProject' | None = None
-    uid: int = 0
-
-    def get(self) -> T:
-        if self.project is None:
-            raise ValueError(f"Field 'project' is None")
-        if self.uid not in self.project.objects_by_uid:
-            raise ValueError(f"Object with uid {self.uid} does not exist")
-        obj: Any = self.project.objects_by_uid[self.uid]
-        if not isinstance(obj, self.obj_type):
-            raise ValueError(f"Object with uid {self.uid} is not of type {self.obj_type}")
-        return obj
-
-@dataclass
 class AtlasImage(HasUID):
     # Reference to the atlas that this image belongs to
-    atlas: UIDRef['Atlas'] = field(default_factory=lambda: UIDRef(Atlas))
+    atlas: 'Atlas' = field(default_factory=lambda: Atlas())
     # Position of top left corner of image within the atlas
     pos: IntCoord = field(default_factory=IntCoord)
     # Optional name for the image to better identify it
