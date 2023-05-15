d2ef1e16bffd0631cdec85bc2f6c95df9d4beafb
RageLeague <34314733+RageLeague@users.noreply.github.com>
2023-05-14 22:41:54 -0400

    Add highlight functionality

--- source/ui/atlas_workspace.py
+++ source/ui/atlas_workspace.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from typing import Optional
-from PIL import ImageTk
+from PIL import ImageTk, Image
 
 from source.model.anim_project import Atlas, AtlasImage, IntCoord
 from source.ui.anim_workspace import AnimWorkspace
@@ -14,10 +14,11 @@ def set_text(entry: ttk.Entry, s: str) -> None:
     entry.insert(tk.END, s)
 
 class AtlasConfigs(ttk.Frame):
-    def __init__(self, atlas: Atlas, workspace: 'AtlasWorkspace', editor: 'editor.AnimEditor', *args, **kwargs) -> None:
+    def __init__(self, atlas: Atlas, workspace: 'AtlasWorkspace', workspace_widget: AnimWorkspace, editor: 'editor.AnimEditor', *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.atlas = atlas
         self.workspace = workspace
+        self.workspace_widget = workspace_widget
         self.editor = editor
 
         ttk.Label(self, text="Pos:").grid(row=0, column=0)
@@ -27,7 +28,7 @@ class AtlasConfigs(ttk.Frame):
         self.pos_y = ttk.Entry(self, width=INPUT_FIELD_WIDTH, validate="key", validatecommand=self.__field_edited)
         self.pos_y.grid(row=0, column=2)
 
-        ttk.Button(self, text="Highlight").grid(row=0, column=3) # TODO: Implement this
+        ttk.Button(self, text="Highlight", command=self.toggle_highlight).grid(row=0, column=3)
 
         ttk.Label(self, text="Size:").grid(row=1, column=0)
         self.size_x = ttk.Entry(self, width=INPUT_FIELD_WIDTH, validate="key", validatecommand=self.__field_edited)
@@ -95,12 +96,20 @@ class AtlasConfigs(ttk.Frame):
         self.save_button.configure(state="normal" if enable else "disabled")
         self.discard_button.configure(state="normal" if enable else "disabled")
 
+    def toggle_highlight(self) -> None:
+        self.workspace.toggle_highlight()
+        self.workspace.render_highlight(self.workspace_widget)
+
 class AtlasWorkspace(WorkspaceController):
     def __init__(self, root: Optional[Atlas] = None, focus: Atlas | AtlasImage | None = None) -> None:
         self.set_root(root)
         self.set_focus(focus)
         self.__cached_focus: Optional[Atlas] = None
         self.config_panel: AtlasConfigs | None = None
+        self.highlight: bool = True
+
+    def toggle_highlight(self) -> None:
+        self.highlight = not self.highlight
 
     def render_atlas(self, canvas: tk.Canvas, loaded_images: list[ImageTk.PhotoImage], atlas: Atlas, offset: IntCoord) -> None:
         pos = atlas.parent_info.pos or IntCoord()
@@ -112,6 +121,31 @@ class AtlasWorkspace(WorkspaceController):
         for child in atlas.children.values():
             self.render_atlas(canvas, loaded_images, child, offset)
 
+    def render_highlight(self, workspace: AnimWorkspace) -> None:
+        workspace.work_canvas.canvas.delete("highlight")
+
+        if not self.highlight:
+            return
+
+        parent: Optional[Atlas] = None
+        pos: IntCoord = IntCoord()
+        size: IntCoord = IntCoord()
+        if isinstance(self.focus, (AtlasImage, Atlas)):
+            size = self.focus.size
+        if isinstance(self.focus, AtlasImage):
+            pos = self.focus.pos
+            parent = self.focus.atlas and self.focus.atlas()
+        else:
+            parent = self.focus
+        while parent and parent != self.root:
+            pos = pos + parent.parent_info.pos
+            parent = parent.parent_info.parent and parent.parent_info.parent()
+        if parent and parent == self.root:
+            pos = pos + parent.parent_info.pos
+            self.highlight_rect = ImageTk.PhotoImage(Image.new("RGBA", size.to_tuple(), "#cc00ff5f"))
+            # workspace.work_canvas.canvas.create_rectangle(pos.x, pos.y, pos.x + size.x, pos.y + size.y, outline="red", width=10, tags=["highlight"])
+            workspace.work_canvas.canvas.create_image(pos.to_tuple(), image=self.highlight_rect, anchor="nw", tags=["highlight"])
+
     def update_workspace(self, workspace: AnimWorkspace, editor: 'editor.AnimEditor') -> None:
         if self.root != self.__cached_focus:
             workspace.work_canvas.canvas.delete("all")
@@ -120,11 +154,13 @@ class AtlasWorkspace(WorkspaceController):
                 self.render_atlas(workspace.work_canvas.canvas, self.loaded_images, self.root, IntCoord())
             workspace.work_canvas.resize_scroll()
 
+        self.render_highlight(workspace)
+
         self.config_panel = None
         workspace.reset_config_panel()
 
         if isinstance(self.focus, Atlas):
-            self.config_panel = AtlasConfigs(self.focus, self, editor, workspace.config_panel, padding=PADDING)
+            self.config_panel = AtlasConfigs(self.focus, self, workspace, editor, workspace.config_panel, padding=PADDING)
             workspace.set_current_name(self.focus.name)
             workspace.set_class_name("Atlas")
             workspace.set_edit_name_fn(lambda s: self.rename_focus(editor, s))
